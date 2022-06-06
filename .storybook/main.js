7a8b02dd5b8928320c9079d863b6fdd77db823b9
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-06-06 22:51:37 +0100

    SB 65 and minor updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  features: {
-    postcss: false,
-  },
   framework: "@storybook/vue3",
   core: {
-    builder: "webpack4",
+    builder: "@storybook/builder-webpack5",
+  },
+  features: {
+    interactionsDebugger: true,
   },
 };
