b408a6ed5aaae7415a5d388183e3b0e17b8d3502
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2024-05-27 16:58:22 +0100

    Deps: Updates package versions

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,13 @@
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-};
+        date: /Date$/
+      }
+    }
+  }
+}
 
-export default preview;
+export default preview
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,48 +1,43 @@
-import MyButton from './Button.vue';
+import { fn } from '@storybook/test'
+import MyButton from './Button.vue'
 
-// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
   title: 'Example/Button',
   component: MyButton,
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-    onClick: {},
-    size: {
-      control: {
-        type: 'select',
-      },
-      options: ['small', 'medium', 'large'],
-    },
+    size: { control: { type: 'select' }, options: ['small', 'medium', 'large'] },
+    backgroundColor: { control: 'color' }
   },
-};
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() }
+}
 
-// More on writing stories with args: https://storybook.js.org/docs/vue/writing-stories/args
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
-  },
-};
+    label: 'Button'
+  }
+}
 
 export const Secondary = {
   args: {
-    label: 'Button',
-  },
-};
+    label: 'Button'
+  }
+}
 
 export const Large = {
   args: {
     size: 'large',
-    label: 'Button',
-  },
-};
+    label: 'Button'
+  }
+}
 
 export const Small = {
   args: {
     size: 'small',
-    label: 'Button',
-  },
-};
+    label: 'Button'
+  }
+}
